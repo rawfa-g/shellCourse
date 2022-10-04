ca43ee1dff010a546c9eb3d5a9ad320a5be930da
rawfa-gee <rafa.gasull.m@gmail.com>
2022-10-04 20:24:01 +0200

    Añadido script 8 sobre validaciones

---
+++ 8_readValidate.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#Programa para ejemplificar como capturar información del usuario y validarla
+#Autor: Rafa Gasull - @rawfa_gee
+
+option=0
+backupName=""
+clave=""
+
+echo "Programa Utilidades Postgres"
+#Acepta el ingreso de información de solo un caracter
+read -n1 -p "Ingresar una opción: " option
+echo -e "\n"
+read -n10 -p "Ingresar nombre del archivo del backup: " backupName
+echo -e "\n"
+echo "Opción: $option, backupName: $backupName"
+read -s -p "Clave: " clave
+echo -e "\nClave: $clave"
