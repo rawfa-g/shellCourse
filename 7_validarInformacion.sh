e86d70adf25b01f15b42a6dc077e17177879f310
Rawfa <rafa.gasull.m@gmail.com>
2022-10-04 19:59:39 +0200

    Ampliación información para futuras lecturas

--- 7_validarInformacion.sh
+++ 7_validarInformacion.sh
@@ -5,6 +5,10 @@
 identificacionRegex="^[0-9]{10}$"
 paisRegex="ES|GR|FR$"
 fechaNacimientoRegex="^(19|20)([0-9]{2})(0[1-9]|1[0-2])(0[1-9]|[1-2][0-9]|3[0-1])$"
+#Esto lo que hace es:
+#Valida que empiece por 19xx o 20xx y que los {2} siguientes valores pueden tener numeros del 0 al 9
+#Despues valida el mes, si empieza por 0 tiene que ir seguido de un 1 al 9 y si empieza por 1 seguido de un numero del 0 al 2
+#Finalmente valida el día con un sistem aparecido al anterior, si es 0x.. si es 1x o 2x.. y si es 3 que solo pueda ser 30 0 31
 
 echo "Expresiones regulares"
 read -p "Ingresar una identificacion: " identificacion
