b4525821542562b6971ae87b8d28016af8299c95
rawfa-gee <rafa.gasull.m@gmail.com>
2022-10-07 18:13:00 +0200

    Script 31 Install/Uninstall postgres

---
+++ 31_installUninstallPostgres.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+#Programa que permite manejar las utilidades de Postres
+#Autor: Rafa Gasull - @rawfa_g
+
+opcion=0
+
+#Función para instalar Postgres
+instalar_postgres () {
+    echo -e "\nVerificar instalación postgres..."
+    verifyInstall=$(which psql)
+    if [ $? -eq 0 ]; then
+        echo "\n Postgres ya se encuentra instalado en el equipo"
+    else
+        read -s -p "Ingresar contraseña de sudo: " password
+        read -s -p "Ingresar contraseña a utilizar en postgres: " postgresPassword
+        echo "$password" | sudo -S apt update
+        echo "$password" | sudo -S apt-get -y install postgresql postgresql-contrib
+        sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD '{$postgresPassword}';"
+        echo "$password" | sudo -S systemctl enable postgresql.service
+        echo "$password" | sudo -S systemctl start postgresql.service
+    fi
+    read -n1 -s -r -p "PRESIONE [ENTER] para continuar..."
+}
+
+#Función para desinstalar Postgres
+desinstalar_postgres () {
+    read -s -p "Ingresar contraseña de sudo: " password
+    echo -e "\n"
+    echo "$password" | sudo -S systemctl stop postgresql.service
+    echo "$password" | sudo -S apt-get -y --purge remove postgresql\*
+    echo "$password" | sudo -S rm -r /etc/postgresql
+    echo "$password" | sudo -S rm -r /etc/postgresql-common
+    echo "$password" | sudo -S rm -r /var/lib/postgresql
+    echo "$password" | sudo -S userdel -r postgres
+    echo "$password" | sudo -S groupdel postgresql
+
+    read -n1 -s -r -p "PRESIONE [ENTER] para continuar..."
+}
+
+#Realizar backup
+realizar_backup () {
+    echo "Realizar Backup"
+    echo "Directorio backup: $1"
+}
+
+#Restaurar Backup"
+restaurar_backup () {
+    echo "Restaurar Backup"
+    echo "Directorio Restauración: $1"
+}
+
+while :
+#while : nos permite hacer un while infinito
+
+do
+    #Limpiar la pantall
+    clear
+    #Desplegar menú de opciones
+    echo "-----------------------------------------"
+    echo "PGUTIL - Programa de Utilidad de Postgres"
+    echo "-----------------------------------------"
+    echo "             MENÚ PRINCIPAL              "
+    echo "-----------------------------------------"
+    echo "1. Instalar Postgres"
+    echo "2. Desinstalar Posgres"
+    echo "3. Realizar backup"
+    echo "4. Restaurar backup"
+    echo "5. Salir"
+
+    #Leer los datos del usuario - capturar información
+    read -n1 -p "Ingrese una opción [1-5]: " opcion
+
+    #Validar la opción ingresada
+    case $opcion in
+        1)
+            instalar_postgres
+            sleep 3
+            ;;
+        2)
+            desinstalar_postgres
+            sleep 3
+            ;;
+        3)
+            read -p "Directorio de Backup: " directorioBackup
+            realizar_backup $directorioBackup
+            sleep 3
+            ;;
+        4)
+            read -p "Directorio de Restauración: " directorioRest
+            restaurar_backup $directorioRest
+            sleep 3
+            ;;
+        5)
+            echo -e "\nSaliendo del programa..."
+            exit 0
+            ;;
+     esac
+done
