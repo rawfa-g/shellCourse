04805ad4156fcfa7507ed5848ac6302b0a092d02
rawfa-gee <rafa.gasull.m@gmail.com>
2022-10-07 11:41:58 +0200

    Script 18 Loops anidados

---
+++ 18_loopAnidado.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#Programa para ejemplificar el uso de los loops anidados
+#Autor: Rafa Gasull - @rawfa_g
+
+echo "Loops Anidados"
+for fil in $(ls)
+do
+   for nombre in {1..4}
+   do
+        echo "Nombre archivo: $fil _ $nombre"
+   done
+done
